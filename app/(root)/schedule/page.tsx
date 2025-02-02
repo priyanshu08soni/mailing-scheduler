126ba9cc71bf0faa5a1fea479a7cfb8e1bba5d3f
PRIYANSHU SONI <122470603+priyanshu08soni@users.noreply.github.com>
2025-02-02 15:19:12 +0530

    commit

--- app/(root)/schedule/page.tsx
+++ app/(root)/schedule/page.tsx
@@ -1,10 +1,13 @@
 import ScheduleForm from "@/components/ScheduleForm";
+import React from 'react'
 
-export default function SchedulePage() {
+const page = () => {
   return (
     <main className="max-w-lg mx-auto mt-10">
       <h1 className="text-2xl font-bold mb-4">Schedule a Mailing</h1>
       <ScheduleForm />
     </main>
-  );
+  )
 }
+
+export default page
