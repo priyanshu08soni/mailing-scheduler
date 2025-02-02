f7b1e9d082cc58a70da08686d57eafd6d643838b
PRIYANSHU SONI <122470603+priyanshu08soni@users.noreply.github.com>
2025-02-02 15:31:33 +0530

    commit

--- app/(root)/schedule/page.tsx
+++ app/(root)/schedule/page.tsx
@@ -1,9 +1,11 @@
+import ScheduleForm from '@/components/ScheduleForm'
 import React from 'react'
 
 const Schedule = () => {
   return (
     <div>
-      
+      <h1 className="text-2xl font-bold mb-4">Schedule a Mailing</h1>
+      <ScheduleForm />
     </div>
   )
 }
