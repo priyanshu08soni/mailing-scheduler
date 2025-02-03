4c46f2b4a46703588b640879b81de867981e6ac4
PRIYANSHU SONI <122470603+priyanshu08soni@users.noreply.github.com>
2025-02-03 14:12:14 +0530

    commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,24 +1,24 @@
 import type { Metadata } from "next";
 import { IBM_Plex_Sans } from "next/font/google";
 import "./globals.css";
-import { twMerge } from "tailwind-merge";
-import clsx, { ClassValue } from "clsx";
+import { cn } from "@/lib/utils";
 import { 
   ClerkProvider
 } from "@clerk/nextjs";
+import React from "react";
+
 const IBMPlex = IBM_Plex_Sans({
   subsets: ["latin"],
   weight: ['400','500','600','700'],
   variable: '--font-ibm-plex'
 })
+
+
 export const metadata: Metadata = {
-  title: "Mail-Scheduler",
-  description: "Mail-Scheduler",
+  title: "Email Scheduler",
+  description: "Email Scheduler",
 };
-// Improved type for the `cn` function
-export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs));
-}
+
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -26,15 +26,15 @@ export default function RootLayout({
 }>) {
   return (
     <ClerkProvider appearance={{
-      variables: {colorPrimary: '#624cf5'}
-    }}>
+        variables:{colorPrimary:'#624cf5'}
+      }}>
       <html lang="en">
-          <body
-            className={`${cn("font-IBMPlex antialiased", IBMPlex.variable)}`}
-            >
-            {children}
-          </body>
-        </html>
+        <body
+          className={`${cn("font-IBMPlex antialiased", IBMPlex.variable)}`}
+          >
+          {children}
+        </body>
+      </html>
     </ClerkProvider>
   );
 }
